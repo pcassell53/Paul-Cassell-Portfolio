c2e58a73daac03c09595e4bd5d7028b6257609aa
pcassell53 <paulcassell221@gmail.com>
2022-08-14 20:05:28 -0400

    Addition of Hamburger Menu Animation
    I've finally added the hamburger menu on mobile that animates to an X button when clicked.

--- Javascript/paulcassellportfolio.js
+++ Javascript/paulcassellportfolio.js
@@ -23,3 +23,15 @@ AOS.init({
     delay: 300,
     disable: 'mobile'
 });
+
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
